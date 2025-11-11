a7bc04506a59485be97a1b744c54f5bf615d1a28
sandramohan0311 <sandramohan0311@gmail.com>
2025-11-11 21:47:25 +0530

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -3,9 +3,10 @@ import os
 from langchain_groq import ChatGroq
 
 load_dotenv()
-
-llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY"),model_name="openai/gpt-oss-safeguard-20b")
+GROQ_API_KEY = "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"
+llm = ChatGroq(groq_api_key=GROQ_API_KEY,model_name="openai/gpt-oss-safeguard-20b")
 
 if __name__ == "__main__":
     response = llm.invoke("what are the two main ingradients in chai?")
+
     print(response.content)
